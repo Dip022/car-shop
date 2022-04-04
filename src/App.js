7a182d5818d93c4ee5edbd3b72e671fd12269255
Dipok Roy <roydipok022@gmail.com>
2022-04-04 10:30:04 +0600

    header added

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
 import "./App.css";
 import { Route, Routes } from "react-router-dom";
+import Home from "./Components/Home/Home";
+import Header from "./Components/Header/Header";
+import Review from "./Components/Review/Review";
+import Dashboard from "./Components/Dashboard/Dashboard";
+import Blogs from "./Components/Blogs/Blogs";
+import About from "./Components/About/About";
+import NotFound from "./Components/NotFound/NotFound";
 
 function App() {
   return (
     <div className="App">
+      <Header></Header>
       <Routes>
-        <Route></Route>
+        <Route path="/" element={<Home></Home>}></Route>
+        <Route path="/home" element={<Home></Home>}></Route>
+        <Route path="/review" element={<Review></Review>}></Route>
+        <Route path="/dashboard" element={<Dashboard></Dashboard>}></Route>
+        <Route path="/blogs" element={<Blogs></Blogs>}></Route>
+        <Route path="/about" element={<About></About>}></Route>
+        <Route path="*" element={<NotFound></NotFound>}></Route>
       </Routes>
     </div>
   );
--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -1,10 +1,15 @@
 import React from "react";
 import { Link } from "react-router-dom";
+import "./Header.css";
 
 const Header = () => {
   return (
-    <nav>
-      <Link></Link>
+    <nav className="nav-bar">
+      <Link to="/home">HOME</Link>
+      <Link to="/review">REVIEW</Link>
+      <Link to="/dashboard">DASHBOARD</Link>
+      <Link to="/blogs">BLOGS</Link>
+      <Link to="/about">ABOUT</Link>
     </nav>
   );
 };
